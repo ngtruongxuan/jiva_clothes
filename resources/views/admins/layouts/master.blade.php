8e59773b8da3bfaa67ab393d4659805952861915
Truong Xuan <truongxuan92vt@gmail.com>
2018-07-22 00:13:20 +0700

    update

--- resources/views/admins/layouts/master.blade.php
+++ resources/views/admins/layouts/master.blade.php
@@ -158,7 +158,7 @@
             <!-- /.modal -->
 
             <!-- Bootstrap-notify -->
-            <div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">
+            {{--<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">
                 <button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>
                 <span data-notify="icon"></span>
                 <span data-notify="title">{1}</span>
@@ -167,7 +167,7 @@
                     <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
                 </div>
                 <a href="{3}" target="{4}" data-notify="url"></a>
-            </div>
+            </div>--}}
         </div>
     </body>
     <script>
